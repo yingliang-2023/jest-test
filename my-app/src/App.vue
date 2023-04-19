47e79f9726f4fbcfce8c53ce57b83a2fe50fcf6f
YING LIANG <y.liang.168@westcliff.edu>
2023-04-18 23:58:45 -0500

    commit 6

--- my-app/src/App.vue
+++ my-app/src/App.vue
@@ -1,26 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <messageComponent msg="This is my first VUE app!"/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MessageComponent from './components/MessageComponent.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+export default{
+  name:"App",
+  components:{
+    MessageComponent
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
